f9501d48c6a2de4a3ba6994a167b18cfd5d11ddd
Ritzy-Rosabel <mipoolugbenga@gmail.com>
2020-02-29 11:45:47 +0100

    Added comment to the project for future reference

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -8,17 +8,22 @@ namespace DependencyInjection
 {
     //Hello everyone, this is my dependency injectyion pproject it imitates a school log book
    
-    // dependency injection involves three differet types of classed
-    // client class
+    // dependency injection involves three differet types of classes
+    //client class
+    //service class
+    //injector class
+    // we have three types of dependency injector
+    //fields/property injector
+   //constructor injector
+   // setter injector
+   // i will be using the constructor injector 
+
+
+    // client class : This class depends on the Service class to function 
     public class SchoolLog
     {
         private IService _service;
-        // we have three types of dependency injector
-        //fields/property injector
-        //constructor injector
-        // setter injector
-        // i will be using the constructor injector 
-
+      
         public SchoolLog(IService service) 
         {
             this._service = service;
@@ -26,12 +31,13 @@ namespace DependencyInjection
             this._service.GetLastName();
         }
     }
+    //interface for the service class
     public interface IService
     {
         void GetFirstName();
         void GetLastName();
     }
-    // service class:  tthis class provides a service to my client class
+    // service class:  This class provides a service to my client class
     public class Student : IService
     {
         public void GetFirstName()
@@ -57,7 +63,7 @@ namespace DependencyInjection
         }
     }
 
-    // injector class 
+    // injector class :This class injects the Service class to the client class
     class Program
     {
         static void Main(string[] args)
